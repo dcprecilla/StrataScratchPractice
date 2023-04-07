d19fc637a59bd550b26b0e38f4cce647cac52287
Darlene C. Precilla <93794855+dcprecilla@users.noreply.github.com>
2023-04-06 22:26:58 -0700

    Update medium questions.sql

--- Data Lemur/medium questions.sql
+++ Data Lemur/medium questions.sql
@@ -18,3 +18,31 @@ from third
 where ranking = 3;
 
 ---------------------------------------------------------------------------------------------------------------------------------------------------------------
+/*Question: Assume you are given the following tables on Walmart transactions and products. Find the number of unique product combinations that are purchased in the same transaction.
+For example, if there are 2 transactions where apples and bananas are bought, and another transaction where bananas and soy milk are bought, my output would be 2 to represent the 2 unique combinations.
+
+Assumptions:
+
+For each transaction, a maximum of 2 products is purchased.
+You may or may not need to use the products table.
+
+THE GOAL: Find the number of unique product combinations purchased in a single transaction
+
+step 1: Do a self join with the transactions table 
+- Put an additional joining condition t1.column > t2.column this helps to create unique combinations of t1.product_id, t2.product_id 
+and ensures that the product IDs are sorted in descending order (you can also use <).
+
+step 2: Next, we will use the DISTINCT and CONCAT() functions to combine both product IDs into a single cell and return the unique combinations.
+
+step 3: Finally, we conduct a count to obtain the number of unique pairs.
+
+FINAL QUERY: */
+
+select 
+COUNT(DISTINCT CONCAT(t1.product_id, t2.product_id))
+from transactions as t1 
+inner join transactions as t2
+on t1.transaction_id = t2.transaction_id 
+and t1.product_id > t2.product_id;
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------------
